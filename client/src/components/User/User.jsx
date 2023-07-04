cf0d3aeda0b1ecf50a1ded342ae738ea3760e337
Mello47 <melvinshuster47@gmail.com>
2023-07-04 14:14:59 +0200

    added User Information to display in Navbar

---
+++ client/src/components/User/User.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./User.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/userReducer";
+
+
+const User = () => {
+    const user = useSelector((state) => state.usuario.currentUser);
+    const dispatch = useDispatch();
+
+    function Logout() {
+        dispatch(logout());
+        window.location.reload();
+    }
+
+    return ( 
+        <div className="userContainer">
+            <h1>User Information</h1>
+            <div className="user">                
+                {/* <img src={product.image1} alt="" /> */}
+                <div className="details">
+                    <p>{user?.username}</p>
+                    <p>{user?.email}</p>
+                </div>
+            </div>
+            <button onClick={Logout}>SIGN OUT</button>
+        </div>       
+    )
+}
+
+export default User
