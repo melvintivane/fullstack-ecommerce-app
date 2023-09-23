d85182770ef1096b3373522ee839dc81b32472ae
Mello47 <melvinshuster47@gmail.com>
2023-09-23 17:14:17 +0200

    Update base url

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:8080/api/";
+const BASE_URL = "https://kruppstore.onrender.com/api/";
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken || "";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
